dd0275759f2442e06a80dc59e443083c7e57c9ab
Legendry0devil <vishnubza08@gmail.com>
2024-09-04 21:00:36 +0530

    Add files via upload

---
+++ 2/number.c
@@ -0,0 +1,11 @@
+//start time= 20.55
+//end time= 20.56
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number");
+    scanf("%d", &n);
+    printf("The provided number is %d\n", n);
+    getch();
+}
